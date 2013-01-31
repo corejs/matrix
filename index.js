4ac48a3654a6ac68d96f4e43bf0552db5c9e3eb1
Pau Fracés <pfraces@gmail.com>
2013-01-31 02:43:12 +0100

    [new] raw, cut, forEach, some

--- index.js
+++ index.js
@@ -1,25 +1,18 @@
-var extend = require("extend");
+var extend = require("extend"),
+    job = require("job"),
+    done = job.done;
 
 var matrix = module.exports = function (config) {
   return new Matrix(config);
 };
 
-var private = {
-  width: 0,
-  height: 0,
-  matrix: []
-};
+var private = {};
 
 var Matrix = function (config) {
+  private.self = this;
   private.width = config.width;
   private.height = config.height;
 
-  /**
-   * TODO
-   * 
-   * *   de momento creo una matriz nueva, pero el resize debe
-   *     actualizar la matriz existente
-   */
   var m = [];
   for (var y = 0; y < private.height; y++) (function () {
     var row = [];
@@ -31,6 +24,37 @@ var Matrix = function (config) {
   private.matrix = m;
 };
 
+Matrix.prototype.raw = function () {
+  return JSON.parse(JSON.stringify(private.matrix));
+};
+
 Matrix.prototype.at = function (args) {
   return private.matrix[args.y][args.x];
+};
+
+Matrix.prototype.cut = function (area) {
+  var m = [];
+  for (var y = area.begin.y; y < area.end.y; y++) {
+    m.push(private.matrix.slice(area.begin.x, area.end.x));
+  }
+  return matrix(m);
+}
+
+Matrix.prototype.forEach = function (handler) {
+  private.matrix.forEach(function (row, y) {
+    row.forEach(function (cell, x) {
+      handler(cell, { x: x, y: y });
+    });
+  });
+};
+
+Matrix.prototype.some = function (handler) {
+  return job(function () {
+    private.self.forEach(function () {
+      if (handler.apply(null, arguments) === true) {
+        done(true);
+      }
+    });
+    return false;
+  });
 };
