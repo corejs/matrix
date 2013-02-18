dcfca20bc89a46a55b6a81fb926f1dabc054fe1f
Pau Fracés <pfraces@gmail.com>
2013-02-19 00:41:58 +0100

    x,y instead of pos

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var type = require('type'),
-    job = require("job");
-
-var done = job.done;
 
 var matrix = module.exports = function (config) {
   return new Matrix(config);
@@ -13,17 +10,13 @@ var Matrix = function (config) {
   type(config).handle({
     'arr': function () {
       matrix.matrix = cloneMatrix(config);
-      matrix.size = {
-        width: config[0].length,
-        height: config.length
-      };
+      matrix.width = config[0].length;
+      matrix.height = config.length;
     },
     'default': function () {
       matrix.matrix = [];
-      matrix.size = {
-        width: config.width,
-        height: config.height
-      };
+      matrix.width = config.width;
+      matrix.height = config.height;
 
       for (var y = 0; y < config.height; y++) (function () {
         var row = [];
@@ -40,14 +33,14 @@ Matrix.prototype.raw = function () {
   return cloneMatrix(this.matrix);
 };
 
-Matrix.prototype.at = function (pos, val) {
-  if (outOfBounds(this.size, pos)) {
+Matrix.prototype.at = function (x, y, val) {
+  if (outOfBounds(this.width, this.height, x, y)) {
     return;
   }
   if (val) {
-    this.matrix[pos.y][pos.x] = val;
+    this.matrix[y][x] = val;
   }
-  return this.matrix[pos.y][pos.x];
+  return this.matrix[y][x];
 };
 
 Matrix.prototype.cut = function (area) {
@@ -61,30 +54,18 @@ Matrix.prototype.cut = function (area) {
 Matrix.prototype.each = function (handler) {
   this.matrix.forEach(function (row, y) {
     row.forEach(function (cell, x) {
-      handler(cell, { x: x, y: y });
+      handler(x, y, cell);
     });
   });
-};
-
-Matrix.prototype.some = function (handler) {
-  var matrix = this;
-  return job(function () {
-    matrix.each(function () {
-      if (handler.apply(null, arguments) === true) {
-        done(true);
-      }
-    });
-    return false;
-  });
-};
-
-var outOfBounds = function (size, pos) {
-  return !(pos.x >= 0 &&
-           pos.y >= 0 && 
-           pos.x < size.width && 
-           pos.y < size.height);
 };
 
 var cloneMatrix = function (m) {
   return JSON.parse(JSON.stringify(m));
+};
+
+var outOfBounds = function (width, height, x, y) {
+  return !(x >= 0 &&
+           y >= 0 && 
+           x < width && 
+           y < height);
 };
